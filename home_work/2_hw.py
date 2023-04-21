bfb1640492a294034e0b22a6d164385a816c79bc
Aleksey <aleksey1334@mail.ru>
2023-04-21 20:45:56 +0300

    commit full project

---
+++ home_work/2_hw.py
@@ -0,0 +1,23 @@
+def task_1() -> None:
+    a: int = 1
+    b: float = 2.5
+    c: str = 'привет'
+    d: list = ['один', 'два', 'три']
+    i: bool = True
+    print(a, b, c, d, i)
+task_1()
+
+def task_2() -> None:
+    a = [1, 2, 3, 5, 8, 13, 21]
+    print(a[0:3])
+
+task_2()
+
+def task_3() -> None:
+    a: int = 3
+    print(a ** 2)
+task_3()
+
+
+
+
